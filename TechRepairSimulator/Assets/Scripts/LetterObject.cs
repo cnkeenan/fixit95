61b1a9580594eebb680cd8e53c401fa93b666678
Alex Marsh <marshalex28@yahoo.com>
2020-01-31 22:26:43 -0500

    Getting Text draw, pushing in case internet goes down again

---
+++ TechRepairSimulator/Assets/Scripts/LetterObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LetterObject : MonoBehaviour
+{
+    public char Letter;
+    private void Awake()
+    {
+        transform.localScale = new Vector2(2, 2);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void SetSprite()
+    {
+        Sprite newSprite = (Sprite)Resources.Load($"Sprites/Letter_{Letter}", typeof(Sprite));
+        GetComponent<SpriteRenderer>().sprite = newSprite;
+        
+    }
+}
---
+++ TechRepairSimulator/Assets/Scripts/TextLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextLoader : MonoBehaviour
+{
+    public LetterObject letterPrefab;
+    private static string testString = "Hi my name is Armand";
+    List<LetterObject> letterObjects = new List<LetterObject>();
+    public Vector3 StartPosition;
+    private Dictionary<char, Sprite> CharacterSprites;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LetterObject firstLetter = Instantiate(letterPrefab, StartPosition, Quaternion.identity);
+        firstLetter.Letter = testString[0];
+        firstLetter.SetSprite();
+        letterObjects.Add(firstLetter);
+        Sprite[] sprites = Resources.LoadAll<Sprite>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
