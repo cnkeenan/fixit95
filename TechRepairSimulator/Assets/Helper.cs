82c9749304e0036d0fbb3b501ec5d96d5fc26138
Vulcan400 <emillopezrrsh@gmail.com>
2020-02-02 15:54:51 -0500

    UI Fixes and Help Implemented

---
+++ TechRepairSimulator/Assets/Helper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helper : MonoBehaviour
+{
+    public GameObject helpBar;
+
+    private void Awake()
+    {
+        if (helpBar != null)
+        {
+            helpBar.SetActive(false);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (helpBar != null)
+        {
+            helpBar.SetActive(true);
+        }
+        else {
+            Debug.Log("Add help bar");
+        }
+    }
+
+    public void closeHelp() {
+        if (helpBar != null)
+        {
+            helpBar.SetActive(false);
+        }
+    } 
+}
